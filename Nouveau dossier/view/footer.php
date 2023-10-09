c2614c00d7c12ebeef1bb157b5c565a63af245cf
Magib <112185392+mageeeb@users.noreply.github.com>
2023-10-09 22:28:07 +0200

    sov 09/10

---
+++ Nouveau dossier/view/footer.php
@@ -0,0 +1,4 @@
+<div class="footer">
+    
+  <h5> ©Design by Magib S. _ 2022-2023 in CF2M</h5>
+ </div>
---
+++ Nouveau dossier/view/liens.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/perso.css">
+    <link rel="stylesheet" href="../public/darkLightMode.css">
+    <script src="../public/js/darkLightMode.js"></script>
+    <title>Liens</title>
+</head>
+<body>
+    <?php
+    include "menu.php"
+    ?>
+    
+      
+<?php
+        include "footer.php"
+    ?>
+     
+
+</body>
+</html>
